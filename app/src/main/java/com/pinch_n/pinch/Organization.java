19536703b6ba2b86d4804a5ad513b48211638201
Kyle Deal <dealie@woh.rr.com>
2015-06-06 15:20:56 -0700

    Made a basic main view. Doesn't load from backend

---
+++ app/src/main/java/com/pinch_n/pinch/Organization.java
@@ -0,0 +1,16 @@
+package com.pinch_n.pinch;
+
+/**
+ * Created by kdeal on 6/6/15.
+ */
+public class Organization {
+    private String name;
+
+    Organization(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
